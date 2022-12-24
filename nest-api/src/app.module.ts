21712a385dd76bb3a69ce98bb31e25886bdc023f
Azraoui <ael-azra@student.1337.ma>
2022-12-24 17:08:08 +0100

    abdellah

--- nest-api/src/app.module.ts
+++ nest-api/src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { AuthController } from './auth/auth.controller';
 import { AuthModule } from './auth/auth.module';
+import { AuthService } from './auth/auth.service';
 
 
 @Module({
   imports: [AuthModule],
-  controllers: [],
-  providers: [],
+  controllers: [AuthController],
+  providers: [AuthService],
 })
-export class AppModule {}
+
+export class AppModule {
+  
+}
--- nest-api/src/auth/auth.controller.ts
+++ nest-api/src/auth/auth.controller.ts
@@ -1,8 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('api/auth/')
 
 export class AuthController {
 
+    constructor (private readonly authService: AuthService) {}
+
+    @Get()
+    getAuth(): string {
+        return "hello world"
+    }
 
 }
