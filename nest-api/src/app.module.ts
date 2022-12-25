fa62a824d2f3adf887ba749cd3bfc3d2d3a0ef3f
Azraoui <ael-azra@student.1337.ma>
2022-12-25 12:17:10 +0100

    abdellah

--- nest-api/src/app.module.ts
+++ nest-api/src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth/auth.controller';
 import { AuthModule } from './auth/auth.module';
-import { AuthService } from './auth/auth.service';
 
 
 @Module({
   imports: [AuthModule],
-  controllers: [AuthController],
-  providers: [AuthService],
+  controllers: [],
+  providers: [],
 })
 
 export class AppModule {
