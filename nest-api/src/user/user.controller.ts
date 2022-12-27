790d0a1e40c26d68bb210029742a5bd942f9d616
Azraoui <ael-azra@student.1337.ma>
2022-12-27 11:06:12 +0100

    abdellah

--- nest-api/src/user/user.controller.ts
+++ nest-api/src/user/user.controller.ts
@@ -1,14 +1,16 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
-import { User } from 'src/auth/decorator';
+import { User } from '@prisma/client';
+import { GetUser } from 'src/auth/decorator';
 import { GaurdService } from 'src/auth/guards';
 
+@UseGuards(GaurdService)
 @Controller('api/users/')
 
 export class UserController {
 
-    @UseGuards(GaurdService)
     @Get('me')
-    getMe(@User() user) {
+    getMe(@GetUser() user: User, @GetUser('email') eml: string) {
+        console.log(eml);
         return user;
     }
 
